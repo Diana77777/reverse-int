3b240489d9cba630b1f73a1b5d843a061fcde6b7
Klimovich <diana_klimovich@bk.ru>
2023-08-11 12:34:29 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-module.exports = function reverse (n) {
-  let reverseNumber = String(Math.abs(n));
-  let resultNumber = reverseNumber.split('').reverse().join(''); 
-  return resultNumber;
+module.exports = function reverse (n) { 
+    let reversedNumber = '';
+    const numberString = Math.abs(n).toString();
+
+    for (let i = numberString.length - 1; i >= 0; i--) {
+      reversedNumber += numberString.charAt(i);
+    }
+
+    return Number(reversedNumber);
+
 }
